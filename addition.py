d87f97df8cfc95cb103f6324c137d1090d60e870
Ewa <155647516+c4linka@users.noreply.github.com>
2024-01-07 09:15:40 +0100

    Add files via upload

--- addition.py
+++ addition.py
@@ -3,5 +3,5 @@ print("A calculator that only adds.")
 a = int(input("First number: "))
 b = int(input("Second number: "))
 
-print("The result of addition is:", a + b)
+print("The result of addition", a, "+", b, "is:", a + b)
 
